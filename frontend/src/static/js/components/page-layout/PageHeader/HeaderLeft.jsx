79ec43a55d3aea9ccf5efb6905ff7c504db4396a
RoboA-PLAYZ <roboandrewplayz@gmail.com>
2026-09-10 19:06:35 -0400

    Add Blue Penny streaming navigation and wordmark

--- frontend/src/static/js/components/page-layout/PageHeader/HeaderLeft.jsx
+++ frontend/src/static/js/components/page-layout/PageHeader/HeaderLeft.jsx
@@ -1,46 +1,56 @@
 import React from 'react';
 import { PageStore } from '../../../utils/stores/';
-import { LinksConsumer, SiteConsumer } from '../../../utils/contexts/';
-import { useLayout, useTheme } from '../../../utils/hooks/';
+import { LinksConsumer } from '../../../utils/contexts/';
+import { useLayout, useUser } from '../../../utils/hooks/';
 import { CircleIconButton } from '../../_shared';
-import { Logo } from './Logo';
 
 export function HeaderLeft() {
-  const { logo } = useTheme();
-
   const { enabledSidebar, toggleMobileSearch, toggleSidebar } = useLayout();
+  const { isAnonymous } = useUser();
 
   return (
-    <SiteConsumer>
-      {(site) => (
-        <LinksConsumer>
-          {(links) => (
-            <div className="page-header-left">
-              <div>
-                <div className="close-search-field">
-                  <CircleIconButton onClick={toggleMobileSearch}>
-                    <i className="material-icons">arrow_back</i>
-                  </CircleIconButton>
-                </div>
-                {enabledSidebar ? (
-                  <div className="toggle-sidebar">
-                    <CircleIconButton onClick={toggleSidebar}>
-                      <i className="material-icons">menu</i>
-                    </CircleIconButton>
-                  </div>
-                ) : null}
-                <Logo src={logo} href={links.home} title={site.title} />
-                {PageStore.get('config-contents').header.onLogoRight ? (
-                  <div
-                    className="on-logo-right"
-                    dangerouslySetInnerHTML={{ __html: PageStore.get('config-contents').header.onLogoRight }}
-                  ></div>
-                ) : null}
-              </div>
+    <LinksConsumer>
+      {(links) => (
+        <div className="page-header-left bp-header-left">
+          <div>
+            <div className="close-search-field">
+              <CircleIconButton onClick={toggleMobileSearch}>
+                <i className="material-icons">arrow_back</i>
+              </CircleIconButton>
             </div>
-          )}
-        </LinksConsumer>
+
+            {enabledSidebar ? (
+              <div className="toggle-sidebar bp-mobile-menu-toggle">
+                <CircleIconButton onClick={toggleSidebar}>
+                  <i className="material-icons">menu</i>
+                </CircleIconButton>
+              </div>
+            ) : null}
+
+            <a className="bp-wordmark" href={links.home} aria-label="Blue Penny home">
+              <span className="bp-wordmark-coin">BP</span>
+              <span className="bp-wordmark-text">BLUE PENNY</span>
+            </a>
+
+            {!isAnonymous ? (
+              <nav className="bp-top-nav" aria-label="Main navigation">
+                <a href={links.home}>Home</a>
+                <a href={links.featured}>Originals</a>
+                <a href={links.archive.categories}>Shows</a>
+                <a href={links.latest}>New Episodes</a>
+                <a href={links.user.liked}>My List</a>
+              </nav>
+            ) : null}
+
+            {PageStore.get('config-contents').header.onLogoRight ? (
+              <div
+                className="on-logo-right"
+                dangerouslySetInnerHTML={{ __html: PageStore.get('config-contents').header.onLogoRight }}
+              ></div>
+            ) : null}
+          </div>
+        </div>
       )}
-    </SiteConsumer>
+    </LinksConsumer>
   );
 }
